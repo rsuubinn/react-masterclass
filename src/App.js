e6c54304e23cc96575d20de7439b7788c8768158
rsuubinn <rsuubinn@gmail.com>
2022-09-24 22:58:41 +0900

    #2.4 Animations and Pseudo Selectors

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
+import { keyframes } from "styled-components";
 import styled from "styled-components";
 
-const Father = styled.div`
+const Wrapper = styled.div`
   display: flex;
 `;
 
-const Input = styled.input.attrs({ required: true })`
+const lotateAnimation = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  50% {
+    transform: rotate(360deg);
+  }
+  100% {
+    transform: rotate(0deg);
+  }
+`;
+
+const Box = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   background-color: tomato;
+  width: 200px;
+  height: 200px;
+  animation: ${lotateAnimation} 1s linear infinite;
+  span {
+    font-size: 30px;
+    &:hover {
+      display: none;
+    }
+  }
 `;
 
 function App() {
   return (
-    <Father>
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-    </Father>
+    <Wrapper>
+      <Box>
+        <span>🥸</span>
+      </Box>
+    </Wrapper>
   );
 }
 
