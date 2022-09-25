b19ad6ce627c6f6de05b204a5e2b263e38f8fccd
rsuubinn <rsuubinn@gmail.com>
2022-09-25 23:57:08 +0900

    #2.7 Themes

--- src/App.js
+++ src/App.js
@@ -1,49 +1,20 @@
-import { keyframes } from "styled-components";
 import styled from "styled-components";
 
 const Wrapper = styled.div`
-  display: flex;
-`;
-
-const lotateAnimation = keyframes`
-  0% {
-    transform: rotate(0deg);
-  }
-  50% {
-    transform: rotate(360deg);
-  }
-  100% {
-    transform: rotate(0deg);
-  }
-`;
-
-const Emoji = styled.span`
-  font-size: 36px;
-`;
-
-const Box = styled.div`
+  background-color: ${(props) => props.backgroundColor};
   display: flex;
   justify-content: center;
   align-items: center;
-  background-color: tomato;
-  width: 200px;
-  height: 200px;
-  animation: ${lotateAnimation} 1s linear infinite;
-  ${Emoji} {
-    font-size: 30px;
-    &:hover {
-      font-size: 96px;
-    }
-  }
+`;
+
+const Title = styled.h1`
+  color: ${(props) => props.textColor};
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box>
-        <Emoji>🥸</Emoji>
-      </Box>
-      <Emoji>🔥</Emoji>
+      <Title>Hello</Title>
     </Wrapper>
   );
 }
