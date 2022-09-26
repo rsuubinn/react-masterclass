eb2721188430da98f2ecfc7d15ee5e2197712e35
rsuubinn <rsuubinn@gmail.com>
2022-09-26 15:00:18 +0900

    #3.1 DefinitelyTyped

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import styled from "styled-components";
 
 const Wrapper = styled.div`
-  background-color: ${(props) => props.backgroundColor};
+  background-color: ${(props) => props.theme.backgroundColor};
   display: flex;
   justify-content: center;
   align-items: center;
+  width: 100%;
+  height: 100vh;
 `;
 
 const Title = styled.h1`
-  color: ${(props) => props.textColor};
+  color: ${(props) => props.theme.textColor};
 `;
 
 function App() {
--- src/index.tsx
+++ src/index.tsx
@@ -5,15 +5,17 @@ import { ThemeProvider } from "styled-components";
 
 const darkTheme = {
   textColor: "whitesmoke",
-  backgroundColor: "#111",
+  backgroundColor: "black",
 };
 
 const lightTheme = {
-  textColor: "#111",
+  textColor: "#black",
   backgroundColor: "whitesmoke",
 };
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={darkTheme}>
