c78538283f4376b6919facc2d3dabe8429eb5569
rsuubinn <rsuubinn@gmail.com>
2022-09-26 23:08:33 +0900

    #3.3 Optional Props

--- src/Circle.tsx
+++ src/Circle.tsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 interface ContainerProps {
   bgColor: string;
+  borderColor: string;
 }
 
 const Container = styled.div<ContainerProps>`
@@ -9,14 +10,16 @@ const Container = styled.div<ContainerProps>`
   height: 200px;
   border-radius: 100px;
   background-color: ${(props) => props.bgColor};
+  border: 2px solid ${(props) => props.borderColor};
 `;
 
 interface CircleProps {
   bgColor: string;
+  borderColor?: string;
 }
 
-function Circle({ bgColor }: CircleProps) {
-  return <Container bgColor={bgColor} />;
+function Circle({ bgColor, borderColor }: CircleProps) {
+  return <Container bgColor={bgColor} borderColor={borderColor ?? bgColor} />;
 }
 
 export default Circle;
