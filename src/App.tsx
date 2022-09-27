929c47be17dac3aa13ab169614c1a2e46ed45248
rsuubinn <rsuubinn@gmail.com>
2022-09-27 20:53:43 +0900

    #4.0 Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,7 @@
-import React, { useState } from "react";
-import styled from "styled-components";
+import Router from "./Router";
 
 function App() {
-  const Container = styled.div`
-    background-color: ${(props) => props.theme.bgColor};
-  `;
-
-  const H1 = styled.h1`
-    color: ${(props) => props.theme.textColor};
-  `;
-
-  return (
-    <Container>
-      <H1>Hello</H1>
-    </Container>
-  );
+  return <Router />;
 }
 
 export default App;
