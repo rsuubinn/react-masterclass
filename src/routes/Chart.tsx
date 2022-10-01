6d1f3856958c59fcdf55d60f67a86771eaf2e8f2
rsuubinn <rsuubinn@gmail.com>
2022-10-01 15:53:01 +0900

    #4.13 Price Chart part Two

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,15 +1,29 @@
 import { useQuery } from "@tanstack/react-query";
 import { fetchCoinHistory } from "../api";
+import ApexChart from "react-apexcharts";
+
+interface IData {
+  time_open: number;
+  time_close: number;
+  open: string;
+  high: string;
+  low: string;
+  close: string;
+  volume: string;
+  market_cap: number;
+}
 
 interface ChartProps {
   coinId: string;
 }
 
 function Chart({ coinId }: ChartProps) {
-  const { isLoading, data } = useQuery(["ohlcv", coinId], () =>
+  const { isLoading, data } = useQuery<IData[]>(["ohlcv", coinId], () =>
     fetchCoinHistory(coinId)
   );
-  return <h1>Chart</h1>;
+  return (
+    <div>{isLoading ? "Loading..." : <ApexChart type="line"></ApexChart>}</div>
+  );
 }
 
 export default Chart;
